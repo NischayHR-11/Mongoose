c645d65e91de06cf9a84ed35819d48ca926fb86d
NISCHAY HR <nischayhr11@gmail.com>
2024-09-03 13:23:15 +0530

    Added Chats Display Route

--- REST/index.js
+++ REST/index.js
@@ -2,11 +2,24 @@ const express=require("express");
 const app=express();
 const port=8080;
 const path=require("path");
+const mongoose=require("mongoose");
 const Chat= require("./models/chats.js");
 
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"/views"));
+app.use(express.static(path.join(__dirname,"/public")));
+
+main().then((res)=>{
+    console.log("connected...");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+    
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
 
 
 app.listen(port,(req,res)=>{
@@ -17,4 +30,13 @@ app.listen(port,(req,res)=>{
 app.get("/",(req,res)=>{
 
     res.send("WHATSAPP DATA BASE CREATED.. ");
+});
+
+app.get("/chats",async(req,res)=>{
+
+
+    let chats=await Chat.find();
+    console.log(chats);
+
+    res.render("chats.ejs",{chats});
 });
