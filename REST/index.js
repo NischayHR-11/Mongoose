c8978f5eb7f8ce01b59ae35e10da89534dc31793
NISCHAY HR <nischayhr11@gmail.com>
2024-09-03 11:51:34 +0530

    Created API Server For Basic Whatsapp Application

---
+++ REST/index.js
@@ -0,0 +1,32 @@
+const { constants } = require("buffer");
+const express=require("express");
+const app=express();
+const port=8080;
+const path=require("path");
+const mongoose=require("mongoose");
+
+main().then((res)=>{
+    console.log("connected...");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main() {
+    
+    await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
+}
+
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+
+app.listen(port,(req,res)=>{
+
+    console.log("Listening TO Port 8080...");
+});
+
+app.get("/",(req,res)=>{
+
+    res.send("WHATSAPP DATA BASE CREATED.. ");
+});
